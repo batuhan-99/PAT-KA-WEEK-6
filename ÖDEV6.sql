8ee5a17cb50acccd0d1a23625ac5eac12f09e248
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-28 02:07:54 +0300

    Add files via upload

---
+++ ÖDEV6.sql
@@ -0,0 +1,4 @@
+--SELECT AVG(rental_rate) AS average_rental_rate FROM film;
+--SELECT COUNT(*) AS film_count FROM film WHERE title LIKE 'C%';
+--SELECT MAX(length) AS max_length FROM film WHERE rental_rate = 0.99;
+--SELECT COUNT(DISTINCT replacement_cost) AS unique_replacement_cost_count FROM film WHERE length > 150;
