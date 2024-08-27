cb681e6aa8f37fcba270710406623a1690be2ded
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-28 02:11:15 +0300

    Add files via upload

---
+++ ÖDEV7.sql
@@ -0,0 +1,3 @@
+--SELECT rating, COUNT(*) AS film_count FROM film GROUP BY rating;
+--SELECT replacement_cost, COUNT(*) AS film_count FROM film GROUP BY replacement_cost HAVING COUNT(*) > 50;
+--SELECT store_id, COUNT(*) AS customer_count FROM customer GROUP BY store_id;
