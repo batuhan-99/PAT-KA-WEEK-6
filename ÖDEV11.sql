1fabc77d436142d15b622ef5bae877dcc1909d07
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-08-28 15:32:24 +0300

    Add files via upload

---
+++ ÖDEV11.sql
@@ -0,0 +1,16 @@
+--SELECT first_name FROM actor
+
+--UNION 
+
+--SELECT first_name FROM customer ORDER BY first_name;
+
+--SELECT first_name 
+--FROM actor
+
+--INTERSECT
+
+--SELECT first_name 
+--FROM customer
+--ORDER BY first_name;
+
+
